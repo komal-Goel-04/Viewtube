5b93ac1f26b27d8421e04ce3fcdc117a85664613
komal-Goel-04 <komalgoel9437@gmail.com>
2024-07-23 20:46:26 +0530

    utils code

---
+++ src/index.js
@@ -0,0 +1,59 @@
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv"
+
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+.then(() => {
+
+    app.on("error", (error) => {
+        console.log("Express is not able to communicate with mongoose ", error);
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at PORT : ${process.env.PORT}`);
+    })
+    
+})
+.catch( (err) => {
+    console.log("MongoDB connection failed !!!", err)
+} )
+
+
+
+
+
+
+
+
+
+
+/*import mongoose from "mongoose"
+import { DB_NAME } from "./constants"
+import express from "express"
+const app = express();
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("Error : ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    }
+    catch ( error ) {
+        console.log("Error : ", error)
+        throw error
+    }
+}) ()
+*/
+
